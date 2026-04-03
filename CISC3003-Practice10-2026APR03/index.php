2ee353899e682ea57375fe4f4582d6e49f1a39d8
DC325629 <dc32562@um.edu.mo>
2026-04-03 15:34:07 +0800

    .

---
+++ CISC3003-Practice10-2026APR03/index.php
@@ -0,0 +1,20 @@
+<head>
+<title>dc325629 Michael Lam PHP</title>
+</head>
+<body>
+<h1>Michael Lam PHP</h1>
+<p>The SHA256 hash of "Charles Severance" is
+<?php
+echo hash('sha256', 'Charles Severance');
+?></p>
+<pre>ASCII ART:
+    *     *
+    **   **
+    * * * *
+    *  *  *
+    *     *
+</pre>
+<a href="check.php">Click here to check the error setting</a>
+<br/>
+<a href="fail.php">Click here to cause a traceback</a>
+</body>
